96894e8816bfe21a4c8719e2ab7666679e17b109
amitkalo <amitkalo1989@gmail.com>
2020-05-11 00:49:38 +0300

    return posts in chunk pieces.

--- functions/src/uploadPost.ts
+++ functions/src/uploadPost.ts
@@ -27,13 +27,15 @@ export const uploadPostModule = function(req, res) {
 			await insertNewPost(uid, categories, description, img_url, timeStamp, feature_name).then(async function(newPostRef){
 				if (querySnapshot.size == 1){
 					// location exist. add the new post to the location.
-					await querySnapshot.docs[0].ref.set({d:{posts: admin.firestore.FieldValue.arrayUnion(newPostRef.id)}},{merge:true});
+					await querySnapshot.docs[0].ref.set({d:{posts:admin.firestore.FieldValue.arrayUnion({ id: newPostRef.id,
+																																																timeStamp: timeStamp})}}, {merge:true});
 				} else if (querySnapshot.size == 0){
 					// need to add new location with the new post.
 					await geocollection.add({
 							name: feature_name,
 							coordinates: new firebase.firestore.GeoPoint(latlng.lat, latlng.lng),
-							posts: [newPostRef.id]
+							posts:[{id:newPostRef.id,
+								timeStamp: timeStamp}]
 					});
 				} else {
 					console.log("error - num of " + feature_name + " location is:" + querySnapshot.size);
